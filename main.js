789363cb8045262c560d20591fbdf8ee58209ef6
PabloTei <pteijeirolopez@gmail.com>
2023-01-25 15:47:01 +0100

    iteration1

---
+++ main.js
@@ -0,0 +1,30 @@
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+
+const showMe = document.querySelector(".showme");
+console.log(showMe);
+
+// 1.2 Usa querySelector para mostrar por consola el botón con la clase .showme
+
+const pillado = document.querySelector("#pillado")
+console.log(pillado);
+
+// 1.3 Usa querySelector para mostrar por consola todos los p
+
+const showP = document.querySelectorAll("p");
+console.log(showP);
+
+// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+
+const showPokemon = document.querySelectorAll(".pokemon");
+console.log(showPokemon);
+
+// 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe".
+
+const showCharacters = document.querySelectorAll("span");
+console.log(showCharacters);
+
+// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe".
+
+const showCharacters1 = document.querySelectorAll("span");
+console.log(showCharacters[2]);
+
